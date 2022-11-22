7ab7a12f363ea93af34550811b9a87849168fa38
ArrayIterator <arrayiterator@gmail.com>
2022-11-22 15:49:53 +0700

    add env

--- public/index.php
+++ public/index.php
@@ -11,10 +11,16 @@ try {
     require_once dirname(__DIR__) . '/app/Routes.php';
     $serverRequestCreator = ServerRequestCreatorFactory::create();
     $request = $serverRequestCreator->createServerRequestFromGlobals();
-    $serverPrams = $request->getServerParams();
-    $request_uri = explode('?', $serverPrams['REQUEST_URI'])[0];
-    $script_name = dirname($serverPrams['SCRIPT_NAME']);
-    if ($request_uri !== $script_name && str_starts_with($request_uri, $script_name)) {
+    $serverParams = $request->getServerParams();
+    $request_uri = explode('?', $serverParams['REQUEST_URI'])[0];
+    $script_name = dirname($serverParams['SCRIPT_NAME']);
+    $basePath = getenv('base_path')?:($serverParams['base_path']??null);
+    if ($basePath) {
+        $basePath = preg_replace('~[\\\/]+~', '/', $basePath);
+        $basePath = ltrim($basePath, '/');
+        $basePath = $basePath ? "/$basePath" : $basePath;
+        $app->setBasePath($basePath);
+    } elseif ($request_uri !== $script_name && str_starts_with($request_uri, $script_name)) {
         $basePath = substr($request_uri, 0, strlen($script_name));
         if ($basePath !== '/' && ($basePath[0]??'') === '/') {
             $app->setBasePath($basePath);
